08c2e1dbffe8e5f90f312a933c247c23b55c3628
Edbo849 <edborthwick84@gmail.com>
2024-11-06 17:03:41 +0000

    Update metadata model

--- esgf_playground_utils/models/kafka.py
+++ esgf_playground_utils/models/kafka.py
@@ -127,7 +127,7 @@ class Metadata(BaseModel):
     Multiple metadata attributes required for ESGF but not part of the STAC payload.
     """
 
-    auth: Auth
+    auth: Auth | AuthData
     publisher: Publisher
     time: datetime
     schema_version: str
