a5335fc08c01619dad093330b55284ea404ed347
Edbo849 <edborthwick84@gmail.com>
2024-11-18 10:54:37 +0000

    Added tests for url model_validators

--- test/unit_test/test_models/test_item.py
+++ test/unit_test/test_models/test_item.py
@@ -53,3 +53,63 @@ class TestESGFItemPropertiesCheckInstanceId(TestCase):
 
         with self.assertRaises(ValueError):
             ESGFItemProperties.check_instance_id(mock_instance)  # type: ignore
+
+
+class TestESGFItemPropertiesCheckCitationURL(TestCase):
+    """
+    Test the functionality of the :py:ref:`models.item.ESGFItemProperties.check_citation_url` method.
+    """
+
+    def test_with_valid_input(self) -> None:
+        """Should validate the citation_url value"""
+
+        mock_instance = Mock(spec=ESGFItemProperties)
+        mock_instance.mip_era = "z"
+        mock_instance.instance_id = "a.b.c.d.e.f.g.h.i.v200202"
+        mock_instance.citation_url = (
+            "http://cera-www.dkrz.de/WDCC/meta/z/a.b.c.d.e.f.g.h.i.v200202.json"
+        )
+
+        ESGFItemProperties.check_citation_url(mock_instance)  # type: ignore
+
+    def test_with_invalid_input(self) -> None:
+        """Should raise a :py:exc:`ValueError`"""
+
+        mock_instance = Mock(spec=ESGFItemProperties)
+        mock_instance.mip_era = "z"
+        mock_instance.instance_id = "a.b.c.d.e.f.g.h.i.v200202"
+        mock_instance.citation_url = (
+            "http://cera-www.dkrz.de/WDCC/meta/a/a.b.c.d.e.f.g.h.i.v200202.json"
+        )
+
+        with self.assertRaises(ValueError):
+            ESGFItemProperties.check_citation_url(mock_instance)  # type: ignore
+
+
+class TestTestESGFItemPropertiesCheckFurtherInfoURL(TestCase):
+    """
+    Test the functionality of the :py:ref:`models.item.ESGFItemProperties.check_further_info_url` method.
+    """
+
+    def test_with_valid_input(self) -> None:
+        """Should validate the further_info_url value"""
+
+        mock_instance = Mock(spec=ESGFItemProperties)
+        mock_instance.instance_id = "a.b.c.d.e.f.g.h.i.v200202"
+        mock_instance.further_info_url = (
+            "https://furtherinfo.es-doc.org/a.b.c.d.e.f.g.h.i.v200202"
+        )
+
+        ESGFItemProperties.check_further_info_url(mock_instance)  # type: ignore
+
+    def test_with_invalid_input(self) -> None:
+        """Should raise a :py:exc:`ValueError`"""
+
+        mock_instance = Mock(spec=ESGFItemProperties)
+        mock_instance.instance_id = "a.b.c.d.e.f.g.h.i.v200202"
+        mock_instance.further_info_url = (
+            "https://furtherinfo.es-doc.org/z.b.c.d.e.f.g.h.i.v200202"
+        )
+
+        with self.assertRaises(ValueError):
+            ESGFItemProperties.check_further_info_url(mock_instance)  # type: ignore
